fcbbdd6a4ff3be9cd88e0de388be17d0ab610643
“Ric” <ricardocarballo@gmail.com>
2017-10-28 18:32:02 +1100

    Completed reveal on scroll effect for featuers and testimonials

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,48 @@
+/*
+Handles feature section animations to reveal elements when the page is being scrolled
+*/
+
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+
+  constructor(element, offset) {
+    /*Access  elements with jquery*/
+    /*itemsToReveal is a collection of items since feature-item class is used by more than one item*/
+    this.itemsToReveal = element;//$(".feature-item, .testimonial");
+    this.offsetPercentage = offset;
+    /*Functions to be executed at page load*/
+    this.hideInitially();
+    this.createWaypoints();
+  
+  }
+ 
+  /*Hide the itmes initially*/
+  hideInitially() {
+    this.itemsToReveal.addClass("reveal-item");
+  }
+
+  /*Create waypoints for each item*/
+  createWaypoints() {
+    //Create a variable to hold this pointer so it can be used wihtin the waypoint creation
+    var revealOnScroll = this;
+    //Execute the following function for each element in itemsToReveal
+    this.itemsToReveal.each(function() {
+      //save this pointer so it can be used within the next object
+      var currentItem = this;
+      //creating a new waypoint object
+      new Waypoint({
+        //Set the watching element to the current item
+        element: currentItem,
+        //Define the handle function once the element is visible via scrolling
+        handler: function() {
+          $(currentItem).addClass("reveal-item--is-visible");
+        },
+        offset: revealOnScroll.offsetPercentage
+      });
+    });
+  }
+}
+
+export default RevealOnScroll;
